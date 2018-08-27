cbde26ca23b14c8206625d1748c9a1c123d9bedc
Eddie James <eajames@us.ibm.com>
2018-08-27 15:41:10 -0500

    Add obmc-ikvm application
    Add license, application source code, autotools recipe, and gitignore
    
    Change-Id: Ice28097a18d25ad3abe056ba0d7d244b5c3e0af2
    Signed-off-by: Eddie James <eajames@us.ibm.com>

---
+++ ikvm_server.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <rfb/rfb.h>
+#include <vector>
+#include "ikvm_args.hpp"
+#include "ikvm_input.hpp"
+#include "ikvm_video.hpp"
+
+namespace ikvm
+{
+
+/*
+ * @class Server
+ * @brief Manages the RFB server connection and updates
+ */
+class Server {
+    public:
+        /*
+         * @struct ClientData
+         * @brief Store necessary data for each connected RFB client
+         */
+        struct ClientData
+        {
+            /*
+             * @brief Constructs ClientData object
+             *
+             * @param[in] s - Number of frames to skip when client connects
+             * @param[in] i - Pointer to Input object
+             */
+            ClientData(int s, Input* i) :
+                skipFrame(s),
+                input(i)
+            {}
+            ~ClientData() = default;
+            ClientData(const ClientData&) = default;
+            ClientData& operator=(const ClientData&) = default;
+            ClientData(ClientData&&) = default;
+            ClientData& operator=(ClientData&&) = default;
+
+            int skipFrame;
+            Input* input;
+        };
+
+        /*
+         * @brief Constructs Server object
+         *
+         * @param[in] args - Reference to Args object
+         * @param[in] i    - Reference to Input object
+         * @param[in] v    - Reference to Video object
+         */
+        Server(const Args& args, Input& i, Video& v);
+        ~Server();
+        Server(const Server&) = default;
+        Server& operator=(const Server&) = default;
+        Server(Server&&) = default;
+        Server& operator=(Server&&) = default;
+
+        /* @brief Resizes the RFB framebuffer */
+        void resize();
+        /* @brief Executes any pending RFB updates and client input */
+        void run();
+        /* @brief Sends pending video frame to clients */
+        void sendFrame();
+
+        /*
+         * @brief Indicates whether or not video data is desired
+         *
+         * @return Boolean to indicate whether any clients need a video frame
+         */
+        inline bool wantsFrame() const
+        {
+            return server->clientHead;
+        }
+        /*
+         * @brief Get the Video object
+         *
+         * @return Reference to the Video object
+         */
+        inline const Video& getVideo() const
+        {
+            return video;
+        }
+
+    private:
+        /*
+         * @brief Handler for a client disconnecting
+         *
+         * @param[in] cl - Handle to the client object
+         */
+        static void clientGone(rfbClientPtr cl);
+        /*
+         * @brief Handler for client connecting
+         *
+         * @param[in] cl - Handle to the client object
+         */
+        static enum rfbNewClientAction newClient(rfbClientPtr cl);
+
+        /* @brief Number of connected clients */
+        int numClients;
+        /* @brief Microseconds to process RFB events every frame */
+        long int processTime;
+        /* @brief Handle to the RFB server object */
+        rfbScreenInfoPtr server;
+        /* @brief Reference to the Input object */
+        Input& input;
+        /* @brief Reference to the Video object */
+        Video& video;
+        /* @brief Default framebuffer storage */
+        std::vector<char> framebuffer;
+};
+
+} // namespace ikvm
---
+++ ikvm_video.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <mutex>
+#include <string>
+#include <vector>
+#include "ikvm_input.hpp"
+
+namespace ikvm
+{
+
+/*
+ * @class Video
+ * @brief Sets up the V4L2 video device and performs read operations
+ */
+class Video {
+    public:
+        /*
+         * @brief Constructs Video object
+         *
+         * @param[in] p     - Path to the V4L2 video device
+         * @param[in] input - Reference to the Input object
+         * @param[in] fr    - desired frame rate of the video
+         */
+        Video(const std::string& p, Input& input, int fr = 30);
+        ~Video();
+        Video(const Video&) = default;
+        Video& operator=(const Video&) = default;
+        Video(Video&&) = default;
+        Video& operator=(Video&&) = default;
+
+        /*
+         * @brief Gets the video frame data
+         *
+         * @return Pointer to the video frame data
+         */
+        char* getData();
+        /* @brief Performs read to grab latest video frame */
+        void getFrame();
+        /*
+         * @brief Gets whether or not the video frame needs to be resized
+         *
+         * @return Boolean indicating if the frame needs to be resized
+         */
+        bool needsResize();
+        /* @brief Resets the video device */
+        void reset();
+        /* @brief Performs the resize and re-allocates framebuffer */
+        void resize();
+        /* @brief Starts streaming from the video device */
+        void start();
+
+        /*
+         * @brief Gets the desired video frame rate in frames per second
+         *
+         * @return Value of the desired frame rate
+         */
+        inline int getFrameRate() const
+        {
+            return frameRate;
+        }
+        /*
+         * @brief Gets the size of the video frame data
+         *
+         * @return Value of the size of the video frame data in bytes
+         */
+        inline size_t getFrameSize() const
+        {
+            return buffers[lastFrameIndex].size;
+        }
+        /*
+         * @brief Gets the height of the video frame
+         *
+         * @return Value of the height of video frame in pixels
+         */
+        inline size_t getHeight() const
+        {
+            return height;
+        }
+        /*
+         * @brief Gets the width of the video frame
+         *
+         * @return Value of the width of video frame in pixels
+         */
+        inline size_t getWidth() const
+        {
+            return width;
+        }
+
+        /* @brief Number of bits per component of a pixel */
+        static const int bitsPerSample;
+        /* @brief Number of bytes of storage for a pixel */
+        static const int bytesPerPixel;
+        /* @brief Number of components in a pixel (i.e. 3 for RGB pixel) */
+        static const int samplesPerPixel;
+
+    private:
+        /*
+         * @struct Buffer
+         * @brief Store the address and size of frame data from streaming
+         *        operations
+         */
+        struct Buffer
+        {
+            Buffer() :
+                data(nullptr),
+                queued(false),
+                size(0)
+            {}
+            ~Buffer() = default;
+            Buffer(const Buffer&) = default;
+            Buffer& operator=(const Buffer&) = default;
+            Buffer(Buffer&&) = default;
+            Buffer& operator=(Buffer&&) = default;
+
+            void* data;
+            bool queued;
+            size_t size;
+        };
+
+        /* @brief Boolean value to indicate if the video device is streaming */
+        bool isStreaming;
+        /* @brief File descriptor for the V4L2 video device */
+        int fd;
+        /* @brief Desired frame rate of video stream in frames per second */
+        int frameRate;
+        /* @brief Buffer index for the last video frame */
+        int lastFrameIndex;
+        /* @brief Height in pixels of the video frame */
+        size_t height;
+        /* @brief Width in pixels of the video frame */
+        size_t width;
+        /* @brief Mutex lock to prevent simultaneous reset and read frame */
+        std::mutex lock;
+        /* @brief Path to the V4L2 video device */
+        std::string path;
+        /* @brief Streaming buffer storage */
+        std::vector<Buffer> buffers;
+};
+
+} // namespace ikvm
