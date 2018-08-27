b5bf48cd5dd5a5e076018a00fe81b58b6bb6124e
Eddie James <eajames@us.ibm.com>
2018-08-27 15:41:10 -0500

    Add obmc-ikvm application
    Add license, application source code, autotools recipe, and gitignore
    
    Change-Id: Ice28097a18d25ad3abe056ba0d7d244b5c3e0af2
    Signed-off-by: Eddie James <eajames@us.ibm.com>

---
+++ obmc-ikvm.cpp
@@ -0,0 +1,11 @@
+#include "ikvm_args.hpp"
+#include "ikvm_manager.hpp"
+
+int main(int argc, char *argv[]) {
+    ikvm::Args args(argc, argv);
+    ikvm::Manager manager(args);
+
+    manager.run();
+
+    return 0;
+}
